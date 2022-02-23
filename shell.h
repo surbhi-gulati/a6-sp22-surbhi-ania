77c235fa9a777d33d255ac65360b9ccd22fa7845
surbhi-gulati <surbhigulati2000@gmail.com>
2022-02-23 18:36:52 +0000

    Task 1 + 2: Headerfile updates

---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include "tokenize.h"
+
+#define MAX_COMLEN 300 /* Can be anything 255+ */
+
+#endif /* ifndef _SHELL_H */
--- tokenize.h
+++ tokenize.h
@@ -6,6 +6,6 @@
 /* Returns a vector of the tokens in the given input string. */
 vect_t* tokenize(char* input);
 
-#define MAX_STRLEN 300
+#define MAX_STRLEN 300 /* Must be at least 255 */
 
 #endif /* ifndef _TOKENIZE_H */
