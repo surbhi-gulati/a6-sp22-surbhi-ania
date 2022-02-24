fba25dee0a334a615edf986be399575c045a0df6
surbhi-gulati <surbhigulati2000@gmail.com>
2022-02-24 20:38:24 +0000

    Task 2: Comments + no exit on nsfod

--- shell.c
+++ shell.c
@@ -61,27 +61,28 @@ int main(int argc, char **argv) {
 		char cmd[MAX_CMDLEN];
 		char* flag = fgets(cmd, MAX_CMDLEN, stdin); // copy given string argument into expr
 		
-		// ctrl + D
+		// ctrl + D command: exit shell
 		if (flag == NULL) {
 			printf("\n");
 			break;
 		}
-		cmd[strcspn(cmd, "\n")] = 0;
 
+		// tokenize and put command args in to a string array
+		cmd[strcspn(cmd, "\n")] = 0;
 		vect_t *command = tokenize(cmd);
 		char** cmd_array = vect_to_str(command);
 
-		// exit
+		// exit command: exit shell
 		if (strcmp(cmd_array[0], "exit") == 0) {
 			break;
 		} 
-		// process cmd
+
+		// process cmd if it is not an exit command
 		else {
 			if (fork() == 0) {
          			if (execvp(cmd_array[0], cmd_array) < 0) {
                        			printf("No such file or directory\n");
                 		}
-                		exit(1);
         		}
         		else {  
                			wait(NULL);
